2ccabcbe6ef079b8f492eea03629ffc36c692efb
David Gage <whytheplatypus@gmail.com>
2013-06-05 17:08:58 -0400

    it works o_0

--- scripts/app.js
+++ scripts/app.js
@@ -1,99 +1,50 @@
 /*global define */
-define(['sjcl', './alert'], function (sjcl, warn) {
+define(['./alert', './chat'], function (warn, Chat) {
     'use strict';
     
-    var Chat = function(){
+    var App = function(peer_options, usersRef){
         var self = this;
         
-        this.mesh = false;
-        this.peer = false;
-        this.password = "password";
-        this.messages = [];
-        
-        //This should be overridden
-        this.onupdate = function(msg){
-            console.log(msg);
+        self.chats = {};
+        if(peer_options.id == ""){
+            console.log(peer_options.key);
+            self.peer = new Peer({key:peer_options.key, debug: false});
+        } else {
+            self.peer = new Peer(peer_options.id, {key:peer_options.key, debug: false});
         }
-        
-        function host(id, opts){
-            
-            if(opts.host !== "" && opts.port !== ""){
-                if(id === ""){
-                    console.log("asking for id");
-                    self.peer = new Peer({host: opts.host, port:opts.port, debug: false});
-                } else {
-                    self.peer = new Peer(id, {host: opts.host, port:opts.port, debug: false});
+        //self.peer = new Peer(peer_options.id, {host:peer_options.host, port:peer_options.port, debug: true});
+        if(self.peer){
+            console.log(self.peer);
+            self.peer.on('error', function(error){
+                console.log(error);
+            });
+            self.peer.on('connection', function setupConnection(conn){
+                self.chats[conn.peer] = new Chat(self.peer, conn, conn.metadata.password);
+                self.onchat(self.chats[conn.peer]);
+            });
+            self.peer.on('open', function(id){
+                document.getElementById('local_id').innerHTML = id;
+                var idRef = usersRef.push(id);
+                window.onbeforeunload = function(){
+                    idRef.remove();
                 }
-            } else if(opts.key !== ""){
-                if(id === ""){
-                    console.log("asking for id with key");
-                    self.peer = new Peer({key:opts.key, debug: true});
-                    self.peer.on("open", function(id){console.log(id);});
-                } else {
-                    self.peer = new Peer(id, {key:opts.key, debug: false});
-                }
-            }
-            if(self.peer){
-                console.log(self.peer);
-                self.peer.on('error', function(error){
-                    console.log(error);
-                });
-                self.peer.on('open', function(id){
-                    console.log(id);
-                    self.mesh = new Mesh(self.peer);
-                    console.log(self.mesh);
-                    self.mesh.on('data', handleData);
-                });
-                return self.peer;
-            } else {
-                console.log("need to fill something out");
-                warn("need to fill something out");
-            }
+            });
+        } else {
+            console.log("need to fill something out");
+            warn("need to fill something out");
         }
         
-        function handleData(data){
-            var text = "";
-            try {
-                text = sjcl.decrypt(self.password, data.message);
-            } catch(e){
-                warn(e);
-            }
-            
-            data.message = text;
-            
-            self.onupdate(data);
-            self.messages.push(data);
-        }
         
-        function connect(id){
-            console.log("connecting to " + id);
-            self.mesh.connect(id);
-        }
         
-        function send(message){
-            //I could add more info to the json..
-            //encript
-            var crypticMessage = message;
-            if(message != "ping")
-                crypticMessage = sjcl.encrypt(self.password, message);
-            
-            var msg = {
-                from: self.mesh.peer.id,
-               //meta: conn.metadata,
-                message:crypticMessage
-            };
-            
-            self.mesh.write(msg);
-            
-            msg.message = message;
-            self.onupdate(msg);
-            self.messages.push(msg);
-        }
+        function _call(id, password){
+            var conn = self.peer.connect(id, {'metadata':{'password': password}});
+            self.chats[conn.peer] = new Chat(self.peer, conn, conn.metadata.password);
+            self.onchat(self.chats[conn.peer]);
+            return self.chats[conn.peer];
+        };
         
-        this.send = send;
-        this.host = host;
-        this.connect = connect;
+        self.call = _call;
     }
     
-    return Chat;
+    return App;
 });
